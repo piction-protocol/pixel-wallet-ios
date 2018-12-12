83040893b4c2130d2e3341acb7c0b8223a54f23d
jhSeo <jh.seo@battleent.com>
2018-12-12 17:31:32 +0900

    add application environment

--- pixelwallet-ios/Src/Config/AppDelegate.swift
+++ pixelwallet-ios/Src/Config/AppDelegate.swift
@@ -18,6 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootView = WalletListViewController.make()
+        let navigationController = UINavigationController(rootViewController: rootView)
+        window?.rootViewController = navigationController
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+
         return true
     }
 
@@ -29,11 +36,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-   }
+
+        //NotificationCenter.default.post(name: .didEnterBackground, object: nil)
+    }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-     }
+        NotificationCenter.default.post(name: .willEnterForeground, object: nil)
+    }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
---
+++ pixelwallet-ios/Src/Config/Storyboard.swift
@@ -0,0 +1,30 @@
+//
+//  Storyboard.swift
+//  pixelwallet-ios
+//
+//  Created by jhseo on 03/12/2018.
+//  Copyright © 2018 Piction Network. All rights reserved.
+//
+
+import UIKit
+
+public enum Storyboard: String {
+    /* Wallet */
+    case WalletList
+    case WalletItem
+    case AddWallet
+    case SelectNetwork
+    case CreateWallet
+    case ImportWallet
+    case WalletInfo
+    case AddWalletComplete
+
+    public func instantiate<VC: UIViewController>(_ viewController: VC.Type) -> VC {
+        guard
+            let vc = UIStoryboard(name: self.rawValue, bundle: nil)
+                .instantiateViewController(withIdentifier: VC.storyboardIdentifier) as? VC
+            else { fatalError("Couldn't instantiate \(VC.storyboardIdentifier) from \(self.rawValue)") }
+
+        return vc
+    }
+}
