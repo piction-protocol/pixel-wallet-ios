76c8c7fc3891c1a7c85b2f2d4960297cef377b28
jhSeo <jh.seo@battleent.com>
2018-12-12 17:31:32 +0900

    add application environment

---
+++ pixelwallet-ios/Src/Config/AppInfo.swift
@@ -0,0 +1,14 @@
+//
+//  AppInfo.swift
+//  pixelwallet-ios
+//
+//  Created by jhseo on 03/12/2018.
+//  Copyright © 2018 Piction Network. All rights reserved.
+//
+
+import UIKit
+
+let SCREEN_W = UIScreen.main.bounds.size.width
+let SCREEN_H = UIScreen.main.bounds.size.height
+
+let keyChainGroupId = "group.com.piction.pixelwallet-ios"
